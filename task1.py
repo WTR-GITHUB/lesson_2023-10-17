b9b73aba30dbd383c998f206b8524fe31620d790
Saulius Anusas <saulius.anusas@gmail.com>
2023-10-17 21:41:48 +0300

    Pirmas kartas

---
+++ task1.py
@@ -0,0 +1,30 @@
+text = "In this lecture we will review some additional functionalities of python built-in data structures."
+# i = 0
+# for leter in text:
+#     if leter == "e":
+#         i += 1
+# print(f"Radziu 'e' tekste yra: {i}")
+
+my_leters = [i for i, v in enumerate(text, start=1) if v == "e"]
+print(f"Radziu 'e' tekste yra: {len(my_leters)}")
+
+word_list = text.split(" ")
+my_words = [i for i, v in enumerate(word_list, start=1) if len(v) > 5]
+print(f"Zodziu ilgesniu nei 5 simboliai yra: {len(my_words)}") 
+
+text = text.replace(" ", "")
+text = text.replace("-", "")
+text = text.replace(".", "")
+text = text.lower()
+my_dict = {}
+
+for leter in text:
+    if leter in my_dict.keys():
+        my_dict[leter] += 1
+    else:
+        my_dict[leter] = 1
+my_dict = sorted(my_dict.items())
+
+print({key : value for key, value in my_dict})
+
+# print(my_dict)
---
+++ task2.py
@@ -0,0 +1,9 @@
+import math
+number = float(input("Please enter a number: "))
+
+if math.sqrt(number) // 1 == math.sqrt(number):
+    print(f"The number is perfect sqare: {math.sqrt(number) // 1}")
+else:
+    print(f"The number is not perfect sqare: {math.sqrt(number)}")
+
+    
